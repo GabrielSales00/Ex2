720d85d6ca3fd399512ab17e3b31371f40113b96
Gabriel Sales <rokkuman78@gmail.com>
2023-08-31 20:33:35 -0300

    Update and rename AddException Handling Best Practices to LogWriteToFile.java

--- LogWriteToFile.java
+++ LogWriteToFile.java
@@ -1,10 +1,15 @@
+/*
+Limpeza de recursos: Crie um programa que abra um arquivo para gravação e grave algum conteúdo nele. Use um bloco try-with-resources para garantir que o arquivo seja fechado corretamente, mesmo se ocorrer uma exceção.
+Log: Integre o log (usando uma biblioteca como Log4j ou o `java.util.logging` embutido) em seu código de tratamento de exceção. Demonstre como o log pode ajudar a fornecer uma visão melhor sobre o que aconteceu durante uma exceção
+*/
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class WriteToFile {
+public class WriteToFileLog {
     private static final Logger logger = LogManager.getLogger(WriteToFile.class);
 
     public static void main(String[] args) {
